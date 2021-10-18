023a8ab0a6b1cce1ae0991e5d1936feea2062dc7
vinteroo <87915148+vinteroo@users.noreply.github.com>
2021-10-18 17:09:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,61 @@
-{
-  "name": "server",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "dev": "nodemon index.js",
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "author": "",
-  "license": "ISC",
-  "dependencies": {
-    "cors": "^2.8.5",
-    "express": "^4.16.4",
-    "ytdl-core": "^0.28.0"
-  },
-  "devDependencies": {
-    "nodemon": "^1.18.10"
-  }
-}
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+
+app.listen(PORT, () => {
+	console.log(`Server Works !!! At port ${PORT}`);
+});
+
+app.get('/downloadmp3', async (req, res, next) => {
+	try {
+		var url = req.query.url;
+		if(!ytdl.validateURL(url)) {
+			return res.sendStatus(400);
+		}
+		let title = 'audio';
+
+		await ytdl.getBasicInfo(url, {
+			format: 'mp4'
+		}, (err, info) => {
+			if (err) throw err;
+			title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+		});
+
+		res.header('Content-Disposition', `attachment; filename="${title}.mp3"`);
+		ytdl(url, {
+			format: 'mp3',
+			filter: 'audioonly',
+		}).pipe(res);
+
+	} catch (err) {
+		console.error(err);
+	}
+});
+
+app.get('/downloadmp4', async (req, res, next) => {
+	try {
+		let url = req.query.url;
+		if(!ytdl.validateURL(url)) {
+			return res.sendStatus(400);
+		}
+		let title = 'video';
+
+		await ytdl.getBasicInfo(url, {
+			format: 'mp4'
+		}, (err, info) => {
+			title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+		});
+
+		res.header('Content-Disposition', `attachment; filename="${title}.mp4"`);
+		ytdl(url, {
+			format: 'mp4',
+		}).pipe(res);
+
+	} catch (err) {
+		console.error(err);
+	}
+});
